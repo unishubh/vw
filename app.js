2c016f0ed6b2164706582cb7f3f772f534ccf96f
root <root@ip-172-31-21-18.us-west-2.compute.internal>
2017-03-10 22:55:55 +0000

    CORS error rectified

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ app.use(passport.initialize());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization,access-control-allow-origin");
+// res.header("Access-Control-Allow-Headers", "*");
+
  res.header("Access-Control-Allow-authorization", "*");
   next();
 });
